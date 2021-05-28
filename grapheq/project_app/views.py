e1f590fec95279ca8fc05516bb4ba5fd3d556765
nabeelraza-7 <razanabeel512@gmail.com>
2021-05-28 22:46:46 +0500

    Created working home page

--- grapheq/project_app/views.py
+++ grapheq/project_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return render(request, 'project_app/index.html')
+
+def plot(request):
+    return render(request, 'project_app/plot.html')
