f7a32c765d879b3d6bfcdce12967318fbe393fcb
nabeelraza-7 <razanabeel512@gmail.com>
2021-05-29 08:10:12 +0500

    User login/regitration system

--- grapheq/project_app/urls.py
+++ grapheq/project_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('plot/', views.plot, name='plot'),
     path('user_login/', views.user_login, name='user_login'),
+    path('user_logout/', views.user_logout, name='user_logout'),
     path('user_registration/', views.user_registration, name='user_registration'),
 ]
--- grapheq/project_app/views.py
+++ grapheq/project_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 def home(request):
     return render(request, 'project_app/index.html')
@@ -8,7 +10,33 @@ def plot(request):
     return render(request, 'project_app/plot.html')
 
 def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR Password is incorrect.')
     return render(request, 'project_app/user_login.html')
 
+def user_logout(request):
+    logout(request)
+    return redirect('user_login')
+
 def user_registration(request):
-    return render(request, 'project_app/user_registration.html')
+    if request.user.is_authenticated:
+        return redirect('home')
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, "Account successfully created for: " + user)
+            return redirect('user_login')
+    context = {'form':form}
+    return render(request, 'project_app/user_registration.html', context)
